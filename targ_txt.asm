f7aa705bb30cbe953db51232bbfa262914e37707
mcspaeth <35144505+mcspaeth@users.noreply.github.com>
2025-03-14 23:15:19 -0400

    HS Display
    High score display screen added

--- targ_txt.asm
+++ targ_txt.asm
@@ -6,7 +6,7 @@ STR01:																					; L1e72
 				.db			"EXTRA POINTS",$00
 
 STR02:																					; L1e7f
-				.db			"EXTENDED PLAY FOR "
+				.db			"EXTENDED PLAY FOR "x
 				
 STR0A:																					; L27e2
 				.db			"PLAYER   ", $00
@@ -24,16 +24,14 @@ STR06:																					; L1f3e
 				.db			"HI SCR", $00
 
 STR07:																					;	L27c7
-;				.db			"END OF GAME", $00							; XXX 
 				.db			"GAME  OVER", $00
 
 STR08:																					; L27d3
+STR1A:
 				.db			"TARG", $00
 
 STR09:																					; L27d8
 				.db			"GET  READY", $00
-
-;				.db			"PLAYER   ", $00								; Was STR0A 
 
 STR0B:																					; L27ec
 				.db			$01,"  10 POINTS", $00
@@ -54,9 +52,11 @@ STR10:																					; L2f25
 				.db			"2 PLAYERS   1 COIN", $00				; Coinage 18
 
 STR11:																					; L2f7f
+STR1E:																					; HS Screen
 				.db			"INSERT  COIN", $00
 
 STR12:																					;	L2f8c
+STR1F:
 				.db			"OR", $00
 
 STR13:																					; L2f9b
@@ -66,14 +66,44 @@ STR14:																					; L2fa1
 				.db			"CREDITS", $00
 
 STR15:																					; L2fc8
-				.db			"COPYRIGHT 1980  BY EXIDY INC", $00
+				.db			"COPYRIGHT ?1980 BY EXIDY INC", $00
 
-STR16:
-STR17:
-				.db			"FREE  PLAY", $00
+STR16:																					; Normal 
+STR17:																					; Top of screen
+				.db			" FREE  PLAY ", $00
 
-STR18:
+STR18:																					; Normal 
+STR26:																					; HS Screen 
+				.db			"            ", $00
+
+STR19:																					; Normal 
+STR25:																					; HS Screen
 				.db			"PRESS  START", $00
+
+STR1B:
+				.db			"ALL TIME HIGH SCORES",$00
+
+STR1C:
+				.db			"RANK   SCORE  NAME",$00
+
+STR1D:
+				.db			"GREAT SCORE",$00
+				
+STR20:
+				.db			"1ST",$00
+
+STR21:
+				.db			"2ND",$00
+
+STR22:
+				.db			"3RD",$00
+
+STR23:
+				.db			"4TH",$00
+
+STR24:
+				.db			"5TH",$00
+
 
 TXTTBL:
 				.dw			STR00
@@ -101,6 +131,20 @@ TXTTBL:
 				.dw			STR16
 				.dw			STR17
 				.dw			STR18
+				.dw			STR19
+				.dw			STR1A
+				.dw			STR1B
+				.dw			STR1C
+				.dw			STR1D
+				.dw			STR1E
+				.dw			STR1F
+				.dw			STR20
+				.dw			STR21
+				.dw			STR22
+				.dw			STR23
+				.dw			STR24
+				.dw			STR25
+				.dw			STR26
 
 				;; Screen locations
 TXTLOC:
@@ -126,10 +170,23 @@ TXTLOC:
 				.dw			$420b														; STR13
 				.dw			$400b														; STR14
 				.dw			$4382 													; STR15
-				.dw			$400b														; STR16
-				.dw			$416b														; STR17
+				.dw			$400a														; STR16
+				.dw			$416a														; STR17
 				.dw			$422a														; STR18
-
+				.dw			$422a														; STR19
+				.dw			$406e														; STR1A
+				.dw			$40c6														; STR1B
+				.dw			$4107														; STR1C
+				.dw			$4000														; STR1D
+				.dw			$42ea														; STR1E
+				.dw			$430f														; STR1F
+				.dw			$4167														; STR20
+				.dw			$41a7														; STR21
+				.dw			$41e7														; STR22
+				.dw			$4227														; STR23
+				.dw			$4267														; STR24
+				.dw			$432a														; STR25
+				.dw			$432a														; STR26
 				
 .if 0
 				;; Remove unused text
